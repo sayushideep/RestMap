53d1c6cb7ce17b73bdc6256b13cd546706a2949c
Ayushi Srivastava <ayushircm47@gmail.com>
2022-06-30 18:26:19 +0530

    Added toolbar functionality

--- source/client/restMap/src/app/app.module.ts
+++ source/client/restMap/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, MapComponent, ToolbarComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
